b8f2e365223db14d9be5c86d13ef201cd2a82c8d
张大川 <dachuanz@gmail.com>
2015-12-30 00:46:10 +0800

    update11

--- src/org/apache/qpid/contrib/json/utils/DeflateUtils.java
+++ src/org/apache/qpid/contrib/json/utils/DeflateUtils.java
@@ -10,211 +10,215 @@ import java.io.OutputStream;
 
 import org.apache.commons.compress.compressors.deflate.DeflateCompressorInputStream;
 import org.apache.commons.compress.compressors.deflate.DeflateCompressorOutputStream;
-
+/**
+ * 
+ * @author zdc52
+ *
+ */
 public class DeflateUtils {
-	public static final int BUFFER = 1024;
+	public static final int BUFFER = 1024 * 8;
 
-    public static final CharSequence EXT = ".zip";
+	public static final CharSequence EXT = ".zip";
 
-    /**
-     * 数据压缩
-     * 
-     * @param data
-     * @return
-     * @throws Exception
-     */
-    public static byte[] compress(byte[] data) throws Exception {
-        ByteArrayInputStream bais = new ByteArrayInputStream(data);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	/**
+	 * 数据压缩
+	 * 
+	 * @param data
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] compress(byte[] data) throws Exception {
+		ByteArrayInputStream bais = new ByteArrayInputStream(data);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-        /*压缩*/
-        compress(bais, baos);
+		/* 压缩 */
+		compress(bais, baos);
 
-        byte[] output = baos.toByteArray();
+		byte[] output = baos.toByteArray();
 
-        baos.flush();
-        baos.close();
+		baos.flush();
+		baos.close();
 
-        bais.close();
+		bais.close();
 
-        return output;
-    }
+		return output;
+	}
 
-    /**
-     * 文件压缩
-     * 
-     * @param file
-     * @throws Exception
-     */
-    public static void compress(File file) throws Exception {
-        compress(file, true);
-    }
+	/**
+	 * 文件压缩
+	 * 
+	 * @param file
+	 * @throws Exception
+	 */
+	public static void compress(File file) throws Exception {
+		compress(file, true);
+	}
 
-    /**
-     * 文件压缩
-     * 
-     * @param file
-     * @param delete
-     *            是否删除原始文件
-     * @throws Exception
-     */
-    public static void compress(File file, boolean delete) throws Exception {
-        FileInputStream fis = new FileInputStream(file);
-        FileOutputStream fos = new FileOutputStream(file.getPath() + EXT);
+	/**
+	 * 文件压缩
+	 * 
+	 * @param file
+	 * @param delete
+	 *            是否删除原始文件
+	 * @throws Exception
+	 */
+	public static void compress(File file, boolean delete) throws Exception {
+		FileInputStream fis = new FileInputStream(file);
+		FileOutputStream fos = new FileOutputStream(file.getPath() + EXT);
 
-        compress(fis, fos);
+		compress(fis, fos);
 
-        fis.close();
-        fos.flush();
-        fos.close();
+		fis.close();
+		fos.flush();
+		fos.close();
 
-        if (delete) {
-            file.delete();
-        }
-    }
+		if (delete) {
+			file.delete();
+		}
+	}
 
-    /**
-     * 数据压缩
-     * 
-     * @param is
-     * @param os
-     * @throws Exception
-     */
-    public static void compress(InputStream is, OutputStream os) throws Exception {
+	/**
+	 * 数据压缩
+	 * 
+	 * @param is
+	 * @param os
+	 * @throws Exception
+	 */
+	public static void compress(InputStream is, OutputStream os) throws Exception {
 
-    	DeflateCompressorOutputStream gos = new DeflateCompressorOutputStream(os);
+		DeflateCompressorOutputStream gos = new DeflateCompressorOutputStream(os);
 
-        int count;
-        byte data[] = new byte[BUFFER];
-        while ((count = is.read(data, 0, BUFFER)) != -1) {
-            gos.write(data, 0, count);
-        }
+		int count;
+		byte data[] = new byte[BUFFER];
+		while ((count = is.read(data, 0, BUFFER)) != -1) {
+			gos.write(data, 0, count);
+		}
 
-        gos.finish();
+		gos.finish();
 
-        gos.flush();
-        gos.close();
-    }
+		gos.flush();
+		gos.close();
+	}
 
-    /**
-     * 文件压缩
-     * 
-     * @param path
-     * @throws Exception
-     */
-    public static void compress(String path) throws Exception {
-        compress(path, true);
-    }
+	/**
+	 * 文件压缩
+	 * 
+	 * @param path
+	 * @throws Exception
+	 */
+	public static void compress(String path) throws Exception {
+		compress(path, true);
+	}
 
-    /**
-     * 文件压缩
-     * 
-     * @param path
-     * @param delete
-     *            是否删除原始文件
-     * @throws Exception
-     */
-    public static void compress(String path, boolean delete) throws Exception {
-        File file = new File(path);
-        compress(file, delete);
-    }
+	/**
+	 * 文件压缩
+	 * 
+	 * @param path
+	 * @param delete
+	 *            是否删除原始文件
+	 * @throws Exception
+	 */
+	public static void compress(String path, boolean delete) throws Exception {
+		File file = new File(path);
+		compress(file, delete);
+	}
 
-    /**
-     * 数据解压缩
-     * 
-     * @param data
-     * @return
-     * @throws Exception
-     */
-    public static byte[] decompress(byte[] data) throws Exception {
-        ByteArrayInputStream bais = new ByteArrayInputStream(data);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	/**
+	 * 数据解压缩
+	 * 
+	 * @param data
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] decompress(byte[] data) throws Exception {
+		ByteArrayInputStream bais = new ByteArrayInputStream(data);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-        /*解压缩*/
+		/* 解压缩 */
 
-        decompress(bais, baos);
+		decompress(bais, baos);
 
-        data = baos.toByteArray();
+		data = baos.toByteArray();
 
-        baos.flush();
-        baos.close();
+		baos.flush();
+		baos.close();
 
-        bais.close();
+		bais.close();
 
-        return data;
-    }
+		return data;
+	}
 
-    /**
-     * 文件解压缩
-     * 
-     * @param file
-     * @throws Exception
-     */
-    public static void decompress(File file) throws Exception {
-        decompress(file, true);
-    }
+	/**
+	 * 文件解压缩
+	 * 
+	 * @param file
+	 * @throws Exception
+	 */
+	public static void decompress(File file) throws Exception {
+		decompress(file, true);
+	}
 
-    /**
-     * 文件解压缩
-     * 
-     * @param file
-     * @param delete
-     *            是否删除原始文件
-     * @throws Exception
-     */
-    public static void decompress(File file, boolean delete) throws Exception {
-        FileInputStream fis = new FileInputStream(file);
-        FileOutputStream fos = new FileOutputStream(file.getPath().replace(EXT, ""));
-        decompress(fis, fos);
-        fis.close();
-        fos.flush();
-        fos.close();
+	/**
+	 * 文件解压缩
+	 * 
+	 * @param file
+	 * @param delete
+	 *            是否删除原始文件
+	 * @throws Exception
+	 */
+	public static void decompress(File file, boolean delete) throws Exception {
+		FileInputStream fis = new FileInputStream(file);
+		FileOutputStream fos = new FileOutputStream(file.getPath().replace(EXT, ""));
+		decompress(fis, fos);
+		fis.close();
+		fos.flush();
+		fos.close();
 
-        if (delete) {
-            file.delete();
-        }
-    }
+		if (delete) {
+			file.delete();
+		}
+	}
 
-    /**
-     * 数据解压缩
-     * 
-     * @param is
-     * @param os
-     * @throws Exception
-     */
-    public static void decompress(InputStream is, OutputStream os) throws Exception {
+	/**
+	 * 数据解压缩
+	 * 
+	 * @param is
+	 * @param os
+	 * @throws Exception
+	 */
+	public static void decompress(InputStream is, OutputStream os) throws Exception {
 
-    	 DeflateCompressorInputStream gis = new  DeflateCompressorInputStream(is);
+		DeflateCompressorInputStream gis = new DeflateCompressorInputStream(is);
 
-        int count;
-        byte data[] = new byte[BUFFER];
-        while ((count = gis.read(data, 0, BUFFER)) != -1) {
-            os.write(data, 0, count);
-        }
+		int count;
+		byte data[] = new byte[BUFFER];
+		while ((count = gis.read(data, 0, BUFFER)) != -1) {
+			os.write(data, 0, count);
+		}
 
-        gis.close();
-    }
+		gis.close();
+	}
 
-    /**
-     * 文件解压缩
-     * 
-     * @param path
-     * @throws Exception
-     */
-    public static void decompress(String path) throws Exception {
-        decompress(path, true);
-    }
+	/**
+	 * 文件解压缩
+	 * 
+	 * @param path
+	 * @throws Exception
+	 */
+	public static void decompress(String path) throws Exception {
+		decompress(path, true);
+	}
 
-    /**
-     * 文件解压缩
-     * 
-     * @param path
-     * @param delete
-     *            是否删除原始文件
-     * @throws Exception
-     */
-    public static void decompress(String path, boolean delete) throws Exception {
-        File file = new File(path);
-        decompress(file, delete);
-    }
+	/**
+	 * 文件解压缩
+	 * 
+	 * @param path
+	 * @param delete
+	 *            是否删除原始文件
+	 * @throws Exception
+	 */
+	public static void decompress(String path, boolean delete) throws Exception {
+		File file = new File(path);
+		decompress(file, delete);
+	}
 }
