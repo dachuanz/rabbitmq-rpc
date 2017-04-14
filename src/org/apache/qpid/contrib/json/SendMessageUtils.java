d91930fd5df795e129116a872fe93817208ff468
张大川 <dachuanz@gmail.com>
2017-04-14 15:38:48 +0800

    Update SendMessageUtils.java

--- src/org/apache/qpid/contrib/json/SendMessageUtils.java
+++ src/org/apache/qpid/contrib/json/SendMessageUtils.java
@@ -1,20 +1,25 @@
 package org.apache.qpid.contrib.json;
 
-import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.qpid.contrib.json.utils.BZip2Utils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.MessageProperties;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 /**
+ * 
  * @author 张大川
- * @since 2015年5月28日
+ *
  */
 public class SendMessageUtils {
 
@@ -27,46 +32,50 @@ public class SendMessageUtils {
 	 *            要传送的对象
 	 * @throws Exception
 	 */
-	public static void sendMessage(String queueName, Object... object)
-			throws Exception {
-		Configuration configuration = new PropertiesConfiguration(
-				"config/rabbitmq.properties");
+	public static void sendMessage(String queueName, Object... object) throws Exception {
+		Configuration config = null;
 		boolean isCompress = false;
-		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost(configuration.getString("hostname"));
-		factory.setUsername(configuration.getString("username"));
-		factory.setPassword(configuration.getString("password"));
-		factory.useNio();
-		if (configuration.containsKey("isCompress"))// 验证
-		{
-			isCompress = configuration.getBoolean("isCompress");
+		Parameters params = new Parameters();
+		FileBasedConfigurationBuilder<FileBasedConfiguration> builder = new FileBasedConfigurationBuilder<FileBasedConfiguration>(
+				PropertiesConfiguration.class).configure(params.properties().setFileName("rabbitmq.properties"));
+		try {
+			config = builder.getConfiguration();
+
+		} catch (ConfigurationException cex) {
+			cex.printStackTrace();
 		}
-		if (configuration.containsKey("port")) {
-			factory.setPort(configuration.getInt("port"));
+		ConnectionFactory factory = new ConnectionFactory();
+		// System.out.println();
+		factory.setHost(config.getString("hostname"));
+		factory.setUsername(config.getString("username"));
+		factory.setPassword(config.getString("password"));
+		factory.useNio();
+		if (config.containsKey("isCompress")) {
+			isCompress = config.getBoolean("isCompress");
+		}
+		if (config.containsKey("port")) {
+			factory.setPort(config.getInt("port"));
 		} else {
 			factory.setPort(AMQP.PROTOCOL.PORT);
 		}
-		
+
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
-		// 声明此队列并且持久化
+
 		channel.queueDeclare(queueName, true, false, false, null);
 
 		String jsonMessage;
 		Object[] messages = object;
 		for (Object object2 : messages) {
-			jsonMessage = JSON.toJSONString(object2,
-					SerializerFeature.WriteClassName);
-			// System.out.println(jsonMessage);
+			jsonMessage = JSON.toJSONString(object2, SerializerFeature.WriteClassName);
 
 			if (isCompress) {
 				channel.basicPublish("", queueName, MessageProperties.PERSISTENT_TEXT_PLAIN,
 						BZip2Utils.compress(jsonMessage.getBytes()));
-			}// 持久化消息
+			} // 持久化消息
 
 			else {
-				channel.basicPublish("", queueName, MessageProperties.PERSISTENT_TEXT_PLAIN,
-						jsonMessage.getBytes());
+				channel.basicPublish("", queueName, MessageProperties.PERSISTENT_TEXT_PLAIN, jsonMessage.getBytes());
 			}
 		}
 
